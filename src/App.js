cbfb7f60672e845ef2cf1d96249f4701311d5dd6
MohfezRahman <mohfezrahman@gmail.com>
2022-08-23 13:55:31 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Iframe from "react-iframe";
+//import IframeResizer from 'iframe-resizer-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Iframe
+        url="https://app.powerbi.com/reportEmbed?reportId=6d439269-dc95-4814-babc-57c0fef82e18&autoAuth=true&ctid=513294a0-3e20-41b2-a970-6d30bf1546fa"
+        width="85%"
+        height="720px"
+        scrolling="no"
+        frameBorder="0"
+        display="initial"
+        position="relative"
+      />
     </div>
   );
 }
